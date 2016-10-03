3d79c0666c1d48850ba0b12f492463e397e7613b
FernandoSeCastro <fernandosecastro@univem.edu.br>
2016-10-03 15:12:13 -0300

    add authors associations controller

--- app/Http/Requests/AuthorRequest.php
+++ app/Http/Requests/AuthorRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ArticleRequest extends FormRequest
+class AuthorRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -16,17 +16,11 @@ class ArticleRequest extends FormRequest
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
-            'title' => 'required|min:3',
-            'content' => 'required',
-            'author_id' => 'required',
+            'name' => 'required',
+            'email' => 'required|email'
         ];
     }
 }
